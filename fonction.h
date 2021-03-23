2e4a96e9f78bb6df665eb3429c42f1eacf0c4f30
Constant Vennin <constant.vennin.etu@univ-lille.fr>
2021-03-23 10:01:21 +0100

    ouais

--- fonction.h
+++ fonction.h
@@ -1,3 +1,6 @@
+
+
 void place_sur_terrain (niveau_t* niveau, int colonne, int ligne, char car);
 void initialise_le_terrain (niveau_t* niveau);
 niveau_t* nouveau_niveau(int nb_colonnes, int nb_lignes);
+char lecture_du_terrain (niveau_t* niveau, int colonne, int ligne);
